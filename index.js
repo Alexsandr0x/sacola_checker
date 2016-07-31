ea899851b32b5a5fee27a576dcb0c987460c221f
Alexsandro Francisco <alexsandro2134@gmail.com>
2016-07-31 15:05:41 -0300

    using test server in cloud9

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 var card_name = "blood-moon"
 
 $.ajax({
-   url:"http://cors.io/?u=https://tappedout.net/mtg-card/"+card_name,
+   url:"https://sacola-checker-alexsandr-01.c9users.io/card/"+card_name,
    type:'GET',
    success: function(data){
-     price_column = $(data).find(".col-sm-4 :nth-child(3)").html();
-     
-     var price = parseFloat( price_column.split(" ").filter( function( el ) {
-       return el != ""
-     })[1].substring(1));
-     console.log(price);
+     console.log(data);
    }
 });
