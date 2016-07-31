86b134459405f8570d04ac2bedc65d52e04a05a1
Alexsandro Francisco <alexsandro2134@gmail.com>
2016-07-31 12:39:28 -0300

    Adicionado testes do codepen

---
+++ index.js
@@ -0,0 +1,14 @@
+var card_name = "blood-moon"
+
+$.ajax({
+   url:"http://cors.io/?u=http://tappedout.net/mtg-card/"+card_name,
+   type:'GET',
+   success: function(data){
+     price_column = $(data).find(".col-sm-4 :nth-child(3)").html();
+     
+     var price = parseFloat( price_column.split(" ").filter( function( el ) {
+       return el != ""
+     })[1].substring(1));
+     console.log(price);
+   }
+});
