a0f1d46cac9560b21e477b6f9cd21b8053e6f3ab
Alexsandro Francisco <alexsandro2134@outlook.com>
2016-08-02 20:44:44 +0000

    set constants to medium, lower and higher price

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var cheerio = require('cheerio');
 var app = express();
 var tappedout_url = 'http://tappedout.net/'
 
+var CARD_PRICES = {
+  LOWER: ".col-sm-4 :nth-child(3)",
+  MEDIUM: ".col-sm-4 :nth-child(2)",
+  HIGHER: ".col-sm-4 :nth-child(4)"
+}
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -24,7 +30,7 @@ app.get('/card/:card_name', function (req, res) {
 			var title, release, rating;
             var json = { 'price' : "", 'card': "", '_error': null};
             try{
-            	price_column = $(html).find(".col-sm-4 :nth-child(3)").html();
+            	var price_column = $(html).find(CARD_PRICES.MEDIUM).html();
 
             	var price = parseFloat( price_column.split(" ").filter( function( el ) {
 		       return el != ""
